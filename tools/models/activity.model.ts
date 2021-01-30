c8f236621df026606f72177c4e052f3b09071566
sinirsizvesinifsiz <cihancan83@gmail.com>
2021-01-31 01:03:47 +0300

    model and dto ts created

---
+++ tools/models/activity.model.ts
@@ -0,0 +1,10 @@
+import { ActivityTypeModel } from './activity-type.model';
+import { AuditModel } from './audit.model';
+
+export  class ActivityModel {
+    id: string;
+    name: string;
+    audit: AuditModel;
+    type: ActivityTypeModel;
+
+}
